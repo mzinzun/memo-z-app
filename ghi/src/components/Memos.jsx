b949a25cd2f768b298a282b8356f8c5aeabec198
mzinzun <zinzunmichael@gmail.com>
2024-06-15 20:58:51 -0700

    configured for AWS ec2 instance

--- ghi/src/components/Memos.jsx
+++ ghi/src/components/Memos.jsx
@@ -3,13 +3,26 @@ import React, { useEffect, useState } from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 function Memos() {
+    const [users, setUsers] = useState([]);
     async function getUsers() {
-        const results = await axios.get('http://localhost:8000/users');
+        const results = await axios.get('http://13.52.104.92:8000/users/');
         const users = results.data;
         console.log('users here!!', users);
+        setUsers(users);
     }
     useEffect(() => {
         getUsers();
     }, []);
+    return (
+        <div>
+            <h1>Sample Users list</h1>
+            <ul >
+                {users.map(user => (
+                    <li key={user.id}>{user.first_name} {user.last_name} username: {user.user_name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 
 }
+export default Memos;
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # memo-z-app
+
+rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.env' -e "ssh -i ~/Documents/aws-memo/memoz-app-calif-macbook.pem" . ubuntu@ec2-13-52-104-92.us-west-1.compute.amazonaws.com:~/app
