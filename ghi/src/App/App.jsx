e7ac65fd4af82854f4aa88413218ebcb54982f87
mzinzun <zinzunmichael@gmail.com>
2024-06-20 12:49:47 -0700

    created coming soon landing page

--- ghi/src/App/App.jsx
+++ ghi/src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import Memos from '../components/Memos';
 import './App.css';
@@ -6,25 +6,48 @@ import './App.css';
 function ComingSoon() {
 
     return (
-        <div>
-            <h1>Coming Soon</h1>
-            <h1>Sample Users list</h1>
-            <Memos />
+        <div className='coming-soon '>
+            <h1 className='bg-info display-1 '>Coming Soon!</h1>
 
+            <p>Check back soon for updates!</p>
         </div>
     );
 }
 
 function App() {
     return (
-        <div>
-            <BrowserRouter>
+        <BrowserRouter>
+        <div className='app'>
+             <header className='px-4'>
+                <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+                    <div className='container'>
+                        <a className='navbar-brand' href='/'>
+                        <img src='../assets/memoZ_logo.png' height='75px' />
+                        </a>
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+                        <div className='collapse navbar-collapse' id='navbarNav'>
+                            <ul className='navbar-nav'>
+                                <li className='nav-item'>
+                                    <a className='nav-link' href='/'>Home</a>
+                                </li>
+                                <li className='nav-item'>
+                                    <a className='nav-link' href='/memos'>Memos</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main>
                 <Routes>
                     <Route path="/" element={<ComingSoon />} />
                 </Routes>
-            </BrowserRouter>
+            </main>
+            <footer className='px-4'>memo-z &copy; 2024</footer>
         </div>
-
+        </BrowserRouter>
     );
 }
 export default App;
--- ghi/src/components/Memos.jsx
+++ ghi/src/components/Memos.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 function Memos() {
     const [users, setUsers] = useState([]);
     async function getUsers() {
-        const results = await axios.get('http://13.52.104.92:8000/users/');
+        const results = await axios.get('http://localhost:8000/users/');
         const users = results.data;
         console.log('users here!!', users);
         setUsers(users);
@@ -13,14 +13,19 @@ function Memos() {
         getUsers();
     }, []);
     return (
-        <div>
-            <h1>Sample Users list</h1>
-            <ul >
-                {users.map(user => (
-                    <li key={user.id}>{user.first_name} {user.last_name} username: {user.user_name}</li>
-                ))}
-            </ul>
-        </div>
+        <main className='row'>
+            <aside className='col-4 border'>
+
+            </aside>
+            <section className='col-8 border memoSec'>
+                <ul >
+                    {users.map(user => (
+                        <li key={user.id}>{user.first_name} {user.last_name} username: {user.user_name}</li>
+                    ))}
+                </ul>
+            </section>
+
+        </main>
     );
 
 }
